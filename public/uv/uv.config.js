1698003e699f561600cbef18df61a3288ba2af78
Damian Swan AAHS <damianmswan@icloud.com>
2024-05-29 19:50:45 -0400

    Test of prefix limits

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,6 +1,6 @@
 /*global Ultraviolet*/
 self.__uv$config = {
-  prefix: "/service/",
+  prefix: "/",
   bare: "/bare/",
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
