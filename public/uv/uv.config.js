467d60915f7ed63849c86f98e1ba8a577e70dbfb
Restyled.io <commits@restyled.io>
2024-05-29 22:57:11 +0000

    Restyled by prettier

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,10 +1,10 @@
 /*global Ultraviolet*/
 self.__uv$config = {
-  prefix : '/service/',
-  bare : '/bare/',
-  handler : '/uv/uv.handler.js',
-  client : '/uv/uv.client.js',
-  bundle : '/uv/uv.bundle.js',
-  config : '/uv/uv.config.js',
-  sw : '/uv/uv.sw.js',
+  prefix: "/service/",
+  bare: "/bare/",
+  handler: "/uv/uv.handler.js",
+  client: "/uv/uv.client.js",
+  bundle: "/uv/uv.bundle.js",
+  config: "/uv/uv.config.js",
+  sw: "/uv/uv.sw.js",
 };
